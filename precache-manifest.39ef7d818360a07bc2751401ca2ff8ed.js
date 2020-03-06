f7ff52e512d7b9ed3f2d105b74a36e12185cacf9
vietmazze <dqnguyen2011@gmail.com>
2020-03-06 16:34:33 -0500

    Updates

---
+++ precache-manifest.39ef7d818360a07bc2751401ca2ff8ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96618fb3534fd25c489693d214c82243",
+    "url": "/productivity-planner/index.html"
+  },
+  {
+    "revision": "cfe1ab74174bc9916a3c",
+    "url": "/productivity-planner/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8bd92fb5bef4285a9ee",
+    "url": "/productivity-planner/static/js/2.02047af0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/productivity-planner/static/js/2.02047af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe1ab74174bc9916a3c",
+    "url": "/productivity-planner/static/js/main.76d7fab0.chunk.js"
+  },
+  {
+    "revision": "e3af7d8140c5bc3f82f5",
+    "url": "/productivity-planner/static/js/runtime-main.6cb0376d.js"
+  }
+]);
