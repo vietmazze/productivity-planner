54a805959b987a6d6433adb58e272cfda2d26de1
vietmazze <dqnguyen2011@gmail.com>
2020-03-06 16:45:15 -0500

    pics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
-
+import gaybear from "./img/gaybear";
+import gayhand from "./img/gayhand";
+import casanovaiq from "./img/casanovaiq";
+import corona from "./img/corona";
+import bitcoin from "./img/bitcoin";
 function App() {
   return (
     <div className='App'>
-      <h1> Productivity planner</h1>
+      <h1>Welcome sir low IQ - Jurko</h1>
+      <p> Quick montage of what this website is about.</p>
+      <img alt='gaybear' />
+      <img alt='gayhand' />
+      <img alt='casanovaiq' />
+      <img alt='corona' />
+      <img alt='bitcoin' />
     </div>
   );
 }
