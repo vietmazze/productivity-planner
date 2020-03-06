f8b0df8492d5d3d97b511e5bbe12f2acff62e08e
vietmazze <dqnguyen2011@gmail.com>
2020-03-06 17:31:57 -0500

    pics6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ function App() {
     <div className='App'>
       <h1>Welcome sir low IQ - Jurko</h1>
       <p> Quick montage of what this website is about.</p>
-      <ReactPlayer url='https://www.youtube.com/watch?v=jyNRZIEYvC8' playing />
+
+      <ReactPlayer
+        className='react-player'
+        url='https://www.youtube.com/watch?v=VhAHnsFugSs'
+        playing
+      />
     </div>
   );
 }
