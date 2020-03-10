8372c764fe3af0830e6a51fece3029f27a53245a
vietmazze <dqnguyen2011@gmail.com>
2020-03-09 23:13:44 -0400

    input ui

--- src/components/input.js
+++ src/components/input.js
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const planInput = () => {
+const Input = () => {
+  const [value, setValue] = useState("");
+  const [rows, setRows] = useState(5);
+  const [minRows, setMinRows] = useState(5);
+  const [maxRows, setMaxRows] = useState(10);
+
+  const handleChange = event => {
+    const textareaLineHeight = 24;
+
+    const previousRows = event.target.rows;
+    setMinRows(event.target.rows); // reset number of rows in textarea
+
+    const currentRows = ~~(event.target.scrollHeight / textareaLineHeight);
+
+    if (currentRows === previousRows) {
+      event.target.rows = currentRows;
+    }
+
+    if (currentRows >= maxRows) {
+      setMaxRows(event.target.rows);
+      event.target.scrollTop = event.target.scrollHeight;
+    }
+
+    setValue(event.target.value);
+    if (currentRows < maxRows) {
+      setRows(currentRows);
+    } else {
+      setRows(maxRows);
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log(rows);
+  //   // eslint-disable-next-line
+  // }, [rows]);
+
   return (
     <div className=''>
       <textarea
         name=''
         id='textInput'
         cols='30'
-        rows='10'
-        className=''
+        rows={rows}
+        value={value}
+        className='textarea'
         placeholder='Create your session'
+        onChange={handleChange}
       ></textarea>
     </div>
   );
 };
 
-export default planInput;
+export default Input;
